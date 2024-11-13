8c582880d786210996cf9509510e925eac4bb585
MateusQ7 <mateusquixada@gmail.com>
2024-11-13 14:20:14 -0300

    feat: add project

--- obj/Debug/net8.0/MyBankBack.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBankBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBankBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+338841b1873e3c374c2fe8bdeb1479b45b7a07e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBankBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBankBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
